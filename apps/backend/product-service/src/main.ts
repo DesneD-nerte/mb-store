e62f21b94ba9b9c5c59f1fb29ae0042fce0e06c2
Aleksandr Ershov <ershov_a@joy.money>
2025-11-27 21:41:06 +0700

    added dockerfile for local development, update ports, gitignore

--- apps/backend/product-service/src/main.ts
+++ apps/backend/product-service/src/main.ts
@@ -12,8 +12,8 @@ async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
+  const port = process.env.PORT || 3002;
+  await app.listen(port, '0.0.0.0');
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
